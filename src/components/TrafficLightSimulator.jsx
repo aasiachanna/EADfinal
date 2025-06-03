21fcf31d723de60efc65f1b5049ce0785801640c
Aasia Channa <aasiachanna05@gmail.com>
2025-06-03 11:19:50 +0500

    Refactor: convert to functional React component

--- src/components/TrafficLightSimulator.jsx
+++ src/components/TrafficLightSimulator.jsx
@@ -18,14 +18,46 @@ const TrafficLightSimulator = () => {
 
 
 return (
-    <div className="traffic-container">
-      <h2>Traffic Light Simulator</h2>
-      
-    </div>
-  );
-};
+    <>
 
+    <style>
+        {
+            .traffic-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 50px;
+          font-family: sans-serif;
+          color: white;
+        }
 
+        .traffic-light {
+          width: 100px;
+          background: #222;
+          border-radius: 20px;
+          padding: 20px 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 20px;
+          box-shadow: inset 0 0 10px #000;
+        }
+
+        .light {
+          width: 60px;
+          height: 60px;
+          border-radius: 50%;
+          background-color: #555;
+          transition: background-color 0.6s, box-shadow 0.6s;
+        }
+
+        }
+    </style>
+    
+    
+    </>
+
+)
 }
 
 export default TrafficLightSimulator;
